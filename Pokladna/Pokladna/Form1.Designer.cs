e1be28e01a70e40b27a50a4ecb7c7d31341859e1
Jiří Hlaváček - JHDELL <jirkahlavacek@centrum.cz>
2020-10-13 09:11:08 +0200

    Vytvořena třída PokladniZaznam

--- Pokladna/Pokladna/Form1.Designer.cs
+++ Pokladna/Pokladna/Form1.Designer.cs
@@ -34,7 +34,7 @@
    // 
    this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
    this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-   this.ClientSize = new System.Drawing.Size(1172, 733);
+   this.ClientSize = new System.Drawing.Size(1079, 859);
    this.Name = "Form1";
    this.Text = "Form1";
    this.ResumeLayout(false);
---
+++ Pokladna/Pokladna/PokladniZaznam.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pokladna
+{
+ public class PokladniZaznam
+ {
+  public int IdPokladniZaznam { get; set; }
+  public int Cislo { get; set; }
+  public DateTime Datum { get; set; }
+  public string Popis { get; set; }
+  public double Castka { get; set; }
+  public double Zustatek { get; set; }
+  public string Poznamka { get; set; }
+
+  public PokladniZaznam()
+  {
+  }
+
+  public PokladniZaznam(int idPokladniZaznam, int cislo, DateTime datum, string popis, double castka, double zustatek, string poznamka)
+  {
+   IdPokladniZaznam = idPokladniZaznam;
+   Cislo = cislo;
+   Datum = datum;
+   Popis = popis;
+   Castka = castka;
+   Zustatek = zustatek;
+   Poznamka = poznamka;
+  }
+ }
+}
